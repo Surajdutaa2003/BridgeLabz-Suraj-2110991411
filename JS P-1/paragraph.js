925c34ea761648c6025d1908544e9cfccf46c37e
Surajdutaa2003 <145049082+Surajdutaa2003@users.noreply.github.com>
2024-12-06 22:25:11 +0530

    Update paragraph.js

--- JS P-1/paragraph.js
+++ JS P-1/paragraph.js
@@ -4,9 +4,13 @@ const paragraph = "This is a test. this is a paragraph";
 const words = paragraph.toLowerCase().replace(/[^\w\s]/g, '').split(' ');
 
 // Calculate word frequency using reduce
-const wordFrequency = words.reduce((acc, word) => ({
-    ...acc,
-    [word]: (acc[word] || 0) + 1
-}), {});
+const wordFrequency = words.reduce(function(acc, word) {
+    if (acc[word]) {
+        acc[word] = acc[word] + 1;
+    } else {
+        acc[word] = 1;
+    }
+    return acc;
+}, {});
 
 console.log(wordFrequency);
