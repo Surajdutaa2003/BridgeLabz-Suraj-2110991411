b4b7c9440c9a25fa15ea25da315241f6705adcb3
Surajdutaa2003 <145049082+Surajdutaa2003@users.noreply.github.com>
2024-12-07 21:21:39 +0530

    Update Reccursion.js

--- Reccursion.js
+++ Reccursion.js
@@ -1,10 +1,10 @@
 function rev(num) {
     // Base case: if n is 0 or 1, return 1 (factorial of 0 and 1 is 1)
-    if (n === 0) {
+    if (num === 0) {
         return 1;
     }
     // Recursive case: factorial of n is n * factorial of (n-1)
-    let y= num * rev(n - 1);
+    let y= num * rev(num - 1);
     console.log(y)
     return y
 }
