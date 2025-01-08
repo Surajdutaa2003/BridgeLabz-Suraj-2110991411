e72213e4f338628856f64299d6e14e696031cb0a
Surajdutaa2003 <Suraj.621dutta@gmail.com>
2025-01-08 11:27:47 +0530

    [Suraj] implemented Dont Repeat Yourself

---
+++ dry.js
@@ -0,0 +1,13 @@
+
+function calculateDiscount(price) {
+    return price * 0.1;
+  }
+  function calculateTax(price) {
+    return price * 0.05;
+  }
+  
+  // Use this:
+  function calculate(price, rate) {
+    return price * rate;
+  }
+  
