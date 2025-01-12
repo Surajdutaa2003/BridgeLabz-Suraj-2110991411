04178a1f3cb69142ca9c01922535e659a6891fbf
Surajdutaa2003 <Suraj.621dutta@gmail.com>
2025-01-12 23:19:12 +0530

    [Suraj] Implementation of Error Handeling

---
+++ ErrorHandeling/E1.js
@@ -0,0 +1,29 @@
+setTimeout(()=>{
+    console.log("Hacking wifi.... Please wait..." ) 
+    }, 1000)
+    
+    
+            setTimeout(()=>{ try{
+                    console.log(rahul)  
+            }
+            catch(err){
+            console.log("Error resolve")
+    }
+            
+            }, 100)
+            
+    
+    
+    
+    
+    setTimeout(()=>{ 
+            console.log("Fetching username and password.... Please wait..." )
+    }, 2000) 
+    
+    setTimeout(()=>{ 
+    console.log("Hacking Rahul's facebook id.... Please wait..." )
+    }, 3000) 
+    
+    setTimeout(()=>{ 
+    console.log("Username and password of Rahul (+919356700001) fetched.... Please wait..." )
+    }, 4000) 
