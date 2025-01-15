ad65aae827387780721cc001d92fa5b861d89a20
Surajdutaa2003 <Suraj.621dutta@gmail.com>
2025-01-15 21:38:20 +0530

    [Suraj] implemented deepcopy

--- Object_And_Classes/deepcopy.js
+++ Object_And_Classes/deepcopy.js
@@ -2,3 +2,5 @@ const original = { name: "Aman", details: { age: 25 } };
 const deepCopy = JSON.parse(JSON.stringify(original));
 
 deepCopy.details.age = 30; 
+console.log(original);
+console.log(deepCopy);
